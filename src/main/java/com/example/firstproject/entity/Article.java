834d2ada39449bc299440187844aee2d10ca7eea
seoyeon <2371013@ewhain.net>
2025-05-25 15:54:03 +0900

    chore: sp3 commit

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -9,21 +9,23 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Entity
 @Getter
+@Entity
 public class Article {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+
     private String title;
-    @Column
     private String content;
 
     public void patch(Article article) {
-        if (article.title != null)
+        if (article.title != null) {
             this.title = article.title;
-        if (article.content != null)
+        }
+        if (article.content != null) {
             this.content = article.content;
+        }
     }
 }
