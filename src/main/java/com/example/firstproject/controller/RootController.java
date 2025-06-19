4d5683bc645e229dc0b2e226240fbd98ef856f2c
seoyeon <2371013@ewhain.net>
2025-06-19 23:00:05 +0900

    chore: sp3 commit

---
+++ src/main/java/com/example/firstproject/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.example.firstproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class RootController {
+    @GetMapping("/")
+    public String index() {
+        return "Spring Boot 애플리케이션이 성공적으로 실행되었습니다.";
+    }
+}
