69e76cc1cb663ba3ccee164fd6f5c16d21193d86
seoyeon <2371013@ewhain.net>
2025-06-20 11:01:27 +0900

    chore: sp3 commit

--- src/main/java/com/example/firstproject/controller/RootController.java
+++ src/main/java/com/example/firstproject/controller/RootController.java
@@ -3,7 +3,7 @@ package com.example.firstproject.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class RootController {
     @GetMapping("/")
     public String index() {
